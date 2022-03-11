ce6daec32ab3cc0c3e72dcc673e0725f67509c59
Tom Sepe <thomas@tomsepe.com>
2022-03-11 09:52:01 -0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>So You Think You&#8217;re An Artist?</title>
+        <title>So You Think Youre An Artist?</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
